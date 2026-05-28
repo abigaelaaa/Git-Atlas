59f46aeb6d5325b9e0663ade05bbd6d32e403aa9
abigaelaaa <dorastinabigaelashamma@gmail.com>
2026-05-28 11:28:39 -0300

    termina modelo userprofille

--- userprofile/models.py
+++ userprofile/models.py
@@ -4,4 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
     user = models .OneToOneField(User, on_delete=models.CASCADE, blank=False,null=False, related_name='userprofile_user_set')
-    picture=models.ImageField(upload_to='userprofile/',blank=True,null=True)
+    picture=models.ImageField(upload_to='userprofile/',blank=True,null=True) 
+    birthdate = models .DateField (blank=True,  null=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'perfil de {self .user.username}'
