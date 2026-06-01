95daf20fd2a2b0308e0841ca162130e19e4c5f2f
Angel <angeldopega@gmail.com>
2026-06-01 12:12:08 -0300

    add routes and viwes

--- product/models.py
+++ product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    code= models.TextField(max_length=20, blank= False, null= False,)
+    name=models.TextField(max_length=250,blank=False,)
+    description = models.TextField(max_length = 500, blank=True, null= True)
+    picture = models.ImageField (upload_to = 'products/',blank= False, null= False)
+    qtt= models.IntegerField(blank= False, null=False)
+    unity= models.TextField( max_length=3, blank= False,  null= False)
+    price= models.DecimalField(max_digits=8, decimal_places=2, blank= False, null= False)
+
+    def __str__(self):
+        return f'{self.code}- {self.name}'
+
