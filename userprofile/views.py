f872be89732bca4bff4afb10f155b3ca27489f8b
abigaelaaa <dorastinabigaelashamma@gmail.com>
2026-06-15 12:13:32 -0300

    fix userprofile views

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,10 @@
 import json
-from django.shortcuts import responsse
-from django.http import HttpResponse
-from djando.import
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from .models import UserProfile
+
 # create your views here.
 def home_view( request ):
     return render(request, 'home.html')
@@ -12,29 +15,32 @@ def userprofile_view(request):
 @csrf_exempt
 def register(request):
     if request.method == 'POST':
-    # 1. transforma o JSON do bruno em um adicionario python
-data = json.loads(request.body)
+        # 1. transforma o JSON do bruno em um adicionario python
+        data = json.loads(request.body)
 
-    #2. cria o user padrão do Django (criptografando a senha)
-nwe_user = user.objects.create_user(
-    username = data['Usernme],' \
-  password = data ['password],
-    email = data['email]
-)
-    #3. cria o userprofie vinculando ao user criado acima
-    new_user_profile = userprofile.object.create(
-        user = new_user ,
-        birthdate = data['birthdate']
+        #2. cria o user padrão do Django (criptografando a senha)
+        new_user = User.objects.create_user(
+            username = data['Username'],
+            password = data ['password'],
+            email = data['email']
+        )
 
-    )
+        #3. cria o userprofie vinculando ao user criado acima
+        new_user_profile = UserProfile.object.create(
+            user = new_user,
+            birthdate = data['birthdate']
 
-    return jsonResponse(
+        )
+
+        return JsonResponse(
+            {
+                'status': 'Sucesso',
+                'message': 'usuario criado!',
+            }, status=201
+        )
+
+    return JsonResponse(
         {
-            'status': 'Sucesso',
-            'message': 'usuario criado!',
-        }, status=201
-    )
-    return jsonResponse(
-        { 'erro': 'metodo não permitido' 
-         status=405
+            'erro': 'metodo não permitido'
+        }, status=405
     )
